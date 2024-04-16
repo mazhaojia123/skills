aadaf781757e853ae0c6305036bc46fd6b364a1e
mazhaojia123 <2832026690@qq.com>
2024-04-16 12:12:56 +0800

    [shfl] shfl_down_sync example

---
+++ hpc-exercises/16_shfl_dwon_reduce/main.cu
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+__global__ void warpReduce() {
+	int lane_id = threadIdx.x & 0x1f;
+	int value = 1;
+	
+	for (int i = 16; i >= 1; i /= 2)
+		value += __shfl_down_sync(0xffffffff, value, i, 32);
+
+	printf("lane_id=%d\tvalue=%d\n", lane_id, value);
+}
+
+int main() {
+	warpReduce<<<1, 32>>>();
+	cudaDeviceSynchronize();
+	return 0;
+}
